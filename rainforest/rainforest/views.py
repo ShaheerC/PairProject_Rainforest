772930ab3c596c4eb2396f84474504e44cb50b23
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-08-02 14:45:40 -0400

    added products and added products html page and base html page

--- rainforest/rainforest/views.py
+++ rainforest/rainforest/views.py
@@ -1,3 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from rainforest.models import *
+
+def products_home(request):
+    context ={'products': Product.objects.all()}
+    response = render(request, 'products.html', context)
+    return HttpResponse(response)
